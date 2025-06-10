2135fe02aa82d04ce162e4cfe73a1f4d5327e4fb
Jordan <jordinholenhart@gmail.com>
2025-06-09 19:48:50 -0700

    completed the not implemented respone exercise. Onto the search!

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,22 @@
+// Exercise: Setting Up a Server
+
 const express = require("express");
 const app = express();
 
+// Creating the initial Message
 app.get("/", function (request, response) {
 
     response.send("Welcome to Chef Marco's Italian Bistro!").end();
 });
 
-// Creating a constant var for the menu array
+//Tell the express app that you want it to listen on port 8080 of your computer
+app.listen(8080, function () {
+
+  //This function gets executed when the app starts listening
+  console.log("Server is listening on 8080");
+});
+
+// Creating a constant variable for the menu array
 const menu = [
     {
       id: 1,
@@ -31,17 +41,15 @@ const menu = [
    ]   
 
 
+   
+// Exercise: Chef Marco needs a menu!
+
+// Using get to bring up the menu array, tested using Postman
 app.get("/menu", function (request, response){
     response.json(menu)
 })
 
-//Tell the express app that you want it to listen on port 8080 of your computer
-app.listen(8080, function () {
-
-    //This function gets executed when the app starts listening
-    console.log("Server is listening on 8080");
-});
-
+// Using get to show the menu items individually based on their Id
 app.get("/menu/:id", function(request, response) {
     const itemId = parseInt(request.params.id)
     const item = menu.find(menuItem => menuItem.id === itemId)
@@ -53,14 +61,16 @@ app.get("/menu/:id", function(request, response) {
     }
 })
 
-app.get('/menu/:id', (req, res) => {
-    const itemId = parseInt(req.params.id);
-    const item = menu.find(menuItem => menuItem.id === itemId);
-  
-    if (item) {
-      res.json(item);
-    } else {
-      res.status(404).json({ error: 'Menu item not found' });
-    }
-  });
-  
+// Exercise: Adding a Not Implemented response
+
+app.use(express.json());
+
+// Using post to post new reservations
+app.post("/reservations", function (request, response) {
+
+  // this shows up in the terminal
+  console.warn("POST /reservations called, but not implemented.")
+
+  // this shows up in Postman
+  response.status(501).send("Route exists but isn’t implemented yet!")
+})
